306fe45dde377499367989771beba5814aa6eb5e
akunot <secastrob@unal.edu.co>
2025-06-19 10:51:40 -0500

    Adicion para el estudio de varios inputs

--- Redes Neuronales/McCulloch_Pitts.py
+++ Redes Neuronales/McCulloch_Pitts.py
@@ -4,24 +4,20 @@ def step_function(x, umbral):
 
 # Implementacion de una neurona de McCulloch-Pitts
 def mcculloch_pitts_neuron(inputs, weights, b, umbral):
-    # Calculo de la suma ponderada de las entradas
     weighted_sum = sum(i * w for i, w in zip(inputs, weights)) + b
-    # Aplicacion de la funcion de activacion
     output = step_function(weighted_sum, umbral)
     return output
 
 # Entradas para la neurona
-inputs = [0, 1]
+inputs_list = [[1, 0], [0, 1], [1, 1], [0, 0]]
 # Pesos para las entradas
-weights = [0.5, 0.5]
+weights_list = [[1, 1], [0, 0]]
 # Termino aditivo
-b = 0.5
+b = 0
 # Umbral para la funcion de activacion
 umbral = 0.6
 
-# Calcular la salida de la neurona de McCoulloch-Pitts
-output = mcculloch_pitts_neuron(inputs, weights, b, umbral)
-print(f"Entradas: {inputs}")
-print(f"Pesos: {weights}")
-print(f"Umbral: {umbral}")
-print(f"Salida de la neurona: {output}")
+for inputs in inputs_list:
+    for weights in weights_list:
+        output = mcculloch_pitts_neuron(inputs, weights, b, umbral)
+        print(f"Entradas: {inputs}, Pesos: {weights}, Salida: {output}")
